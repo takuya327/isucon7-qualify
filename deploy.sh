baf204cea87ae4e5c79bdd773831d351b959dd1c
Takuya Fujii <fujii@talknote.com>
2018-09-08 14:42:24 +0900

    Fix BUG of deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,12 +7,12 @@ EOS`
 USER=isucon
 SRC_DIR=`pwd`
 
-DEST_DIR=/home/isucon/isubata/webapp/go/src/isubata
+DEST_DIR=/home/isucon/isubata
 
 for HOST in $TARGET_HOSTS; do
 	echo "rsync to ${HOST}"
-	rsync -arq --delete-after --exclude=".git" --exclude=".gitignore" --exclude="deploy.sh" \
-		${SRC_DIR} ${USER}@${HOST}:${DEST_DIR}/
+	rsync -ar --exclude=".git" --exclude=".gitignore" --exclude="deploy.sh" \
+		${SRC_DIR}/* ${USER}@${HOST}:${DEST_DIR}/
 	echo "restart isubata.golang.service on ${HOST}"
 	ssh ${USER}@${HOST} sudo systemctl restart isubata.golang.service
 done
